accd67488b470282cdf0bd7ad7da931d6fa675b2
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-02-08 09:28:37 -0300

    Aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+let vel = 86;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+
+if(vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+console.log(`Dirija sempre usado cinto de segurança!`);
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+let país = "Brasil";
+
+console.log(`Vivendo em ${país}`);
+
+if (país === "Brasil") {
+  console.log("você é Brasileiro!");
+} else {
+  console.log("Você é Estrangeiro!");
+}
